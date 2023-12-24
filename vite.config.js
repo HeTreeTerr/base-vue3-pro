faedcf5103cd2b721fc18ab5899e65512e1cf355
HeTreeTerr <3110708879@qq.com>
2023-12-24 10:41:32 +0800

    element-plus全部和手动导入 modified:   package.json modified:   vite.config.js .vscode/

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+//默认写法
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -5,3 +6,32 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+
+/*
+2）element-plus按需引入
+*/
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     AutoImport({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//     Components({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//   ],
+// })
+
+/*
+验证：
+修改src\components\HelloWorld.vue中的
+<button>成<el-button>，保存文件后观察页面上的按钮样式改变
+
+*/
